d91333cd253aa03ddeffb630a382943e3e5607b7
Dinko Pehar <dinko@pehar.dev>
2023-09-15 14:56:45 +0200

    Remove command execution $ on fish script

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,10 +1,10 @@
 # Random fish color
 # fish_logo (for i in (seq 3); random choice (set_color --print-colors); end)
 pfetch # Display information about OS
-head -c $(tput cols) < /dev/zero | tr '\0' '='
+head -c (tput cols) < /dev/zero | tr '\0' '='
 fortune -sn 79 # Display fortune with limit of 79 characters
 # Divider, n times '=' width of terminal and color it
-head -c $(tput cols) < /dev/zero | tr '\0' '='
+head -c (tput cols) < /dev/zero | tr '\0' '='
 echo ""
 
 # Say the fortune on OSX :=)
