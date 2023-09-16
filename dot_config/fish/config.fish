93ab02d8796f46af0d199d0bddd105d0892b650d
Dinko Pehar <dinko@pehar.dev>
2023-09-16 20:48:13 +0200

    Add PNPM configuration

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,1 +1,8 @@
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/raziel/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
