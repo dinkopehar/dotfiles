8e12b3b0e2d69afac4fa079625eab2f844d53b79
Dinko Pehar <dinko@pehar.dev>
2023-09-22 15:29:34 +0200

    Apply Windows Configuration

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,1 @@
+oh-my-posh init pwsh | Invoke-Expression
---
+++ dot_config/winfetch/config.ps1
@@ -0,0 +1,38 @@
+# ===== WINFETCH CONFIGURATION =====
+
+$image = "~/Billy.jpg"
+# $noimage = $true
+# Display image using ASCII characters
+$ascii = $false
+
+# Set the version of Windows to derive the logo from.
+# $logo = "Windows XP"
+# Make the logo blink
+# $blink = $true
+
+@(
+    # "title"
+    # "dashes"
+    # "computer"
+    # "kernel"
+    # "motherboard"
+    # # "custom_time"  # use custom info line
+    # "uptime"
+    # # "ps_pkgs"  # takes some time
+    "pwsh"
+    "dashes"
+    # "terminal"
+    # # "theme"
+    # "cpu"
+    # "gpu"
+    # # "cpu_usage"
+    # "memory"
+    # "disk"
+    # # "battery"
+    # # "locale"
+    # # "weather"
+    # # "local_ip"
+    # # "public_ip"
+    "blank"
+    "colorbar"
+)
