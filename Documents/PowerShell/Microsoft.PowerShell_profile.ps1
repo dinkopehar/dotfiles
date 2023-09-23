af229df1e2a41ea64c19455ba18fdff1f2645727
Dinko Pehar <dinko@pehar.dev>
2023-09-23 14:32:54 +0200

    Update windows specific configs

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,33 @@
-oh-my-posh init pwsh | Invoke-Expression
+Invoke-Expression (&starship init powershell)
+winfetch
+
+Remove-Alias cat
+Remove-Alias ls
+
+function cat {
+    bat $args
+}
+
+function ccat {
+    bat --style plain $args
+}
+
+function l {
+    eza --icons --long --header --all $args
+}
+
+function ls {
+    eza --icons --long --header $args
+}
+
+function tree {
+    eza --icons --tree --level=2 $args
+}
+
+function which($name) {
+        Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function .. {
+    Set-Location -Path ".."
+}
