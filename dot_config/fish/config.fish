12b6a8afd33039cb8820b7945d90321dfd07ea92
Dinko Pehar <dinko@pehar.dev>
2023-10-18 20:13:52 +0200

    Update fish shell config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,8 +1,14 @@
 starship init fish | source
 
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
+
 # pnpm
 set -gx PNPM_HOME "/home/raziel/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
